8badb9ccca196ae8ab4361dc676440ef7454b5f7
spirokeet <chrisc5891@gmail.com>
2022-11-09 13:27:34 +0000

    adding sound files

--- script.js
+++ script.js
@@ -4,5 +4,6 @@ window.addEventListener('keydown', function (e){
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     console.log(audio)
     if(!audio)return; //stop the function from running all together
+    audio.currentTime = 0; //rewinds keypress to the start
     audio.play()
 });
