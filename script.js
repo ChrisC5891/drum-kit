270aaf2f0d07684669d1504f7fa82de030057de0
spirokeet <chrisc5891@gmail.com>
2022-11-09 17:42:20 +0000

    add the .playing animation to keys

--- script.js
+++ script.js
@@ -2,8 +2,10 @@
 window.addEventListener('keydown', function (e){
     console.log(e);
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
     console.log(audio)
     if(!audio)return; //stop the function from running all together
     audio.currentTime = 0; //rewinds keypress to the start
-    audio.play()
+    audio.play();
+    key.classList.add('playing');
 });
