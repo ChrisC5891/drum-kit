e483977714783043b614cd6d8ac73f7c851b0fce
spirokeet <chrisc5891@gmail.com>
2022-11-09 12:45:19 +0000

    adding eventListener

---
+++ script.js
@@ -0,0 +1,8 @@
+//listen for 'keydown' event; runs function below, gives us event
+window.addEventListener('keydown', function (e){
+    console.log(e);
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    console.log(audio)
+    if(!audio)return; //stop the function from running all together
+    audio.play()
+});
